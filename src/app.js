21bdd079c745fd3c7e5c4c9b3699e650306f8c41
James Rock <hello@jamesrock.me>
2026-02-07 15:26:16 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import './app.css';
 
 const app = document.querySelector('#app');
 const MIN = 1;
-const MAX = 7;
-const COUNT = 4;
+const MAX = 10;
+const COUNT = 5;
 const random = (min, max) => (Math.floor(Math.random()*((max-min)+1))+min);
 const getRandom = () => random(MIN, MAX);
 const formatter = new Intl.NumberFormat('en-GB');
@@ -26,43 +26,55 @@ const createContainer = (label = '{label}') => {
 };
 
 const renderTotal = () => {
-  display.innerText = formatNumber(target.value-getTotal());
+  const total = settings.value-getTotal();
+  if(total===0) {
+    sliders.forEach((slider) => {
+      slider.disabled = true;
+    });
+    setTimeout(() => {
+      gameOver.dataset.active = true;
+    }, 500);
+  };
+  display.innerText = formatNumber(total);
 };
 
-const getMultipliers = () => {
-  const bob = [];
-  sliders.forEach(() => {
-    bob.push(random(1, 500));
-  });
-  return bob;
-};
-
-const generateTarget = () => {
+const generateSettings = () => {
   let value = 0;
   const values = [];
+  const multipliers = [];
+  const setters = [];
   sliders.forEach((slider, index) => {
+    multipliers.push(random((index)*10+1, ((index)*10)+10));
     values.push(getRandom());
-    value += values[values.length-1] * multipliers[index];
+    setters.push(getRandom());
+    value += (values[index] * multipliers[index]);
   });
   return {
     value,
-    values
+    values,
+    multipliers,
+    setters
   };
 };
 
 const getTotal = () => {
   let bob = 0;
   sliders.forEach((slider, index) => {
-    bob += slider.value * multipliers[index];
+    bob += (slider.value * settings.multipliers[index]);
+  });
+  return bob;
+};
+
+const getTotalTest = (values) => {
+  let bob = 0;
+  values.forEach((value, index) => {
+    bob += (value * settings.multipliers[index]);
   });
   return bob;
 };
 
 const sliders = Array.from({length: COUNT}).map(() => createSlider());
-const multipliers = getMultipliers();
-const target = generateTarget();
-console.log(multipliers);
-console.log(target);
+let settings = null;
 
 const board = createContainer('board');
 app.appendChild(board);
@@ -74,12 +86,39 @@ board.appendChild(display);
 const holder = createContainer('holder');
 board.appendChild(holder);
 
-sliders.forEach((slider) => {
+const gameOver = createContainer('game-over');
+gameOver.dataset.active = false;
+gameOver.innerHTML = `<h2>Well done!</h2><p>Tap to start new game.</p>`;
+board.appendChild(gameOver);
+
+const newGame = () => {
+  
+  settings = generateSettings();
+
+  console.log('getTotalTest', getTotalTest([1, 2, 3, 4]));
+  console.log('getTotalTest', getTotalTest([4, 3, 2, 1]));
+  console.log(settings);
+
+  sliders.forEach((slider, index) => {
+    slider.value = settings.setters[index];
+    slider.disabled = false;
+  });
+
+  gameOver.dataset.active = false;
+  
+  renderTotal();
+
+};
+
+sliders.forEach((slider, index) => {
   holder.appendChild(slider);
-  slider.value = getRandom();
   slider.addEventListener('input', () => {
     renderTotal();
   });
 });
 
-renderTotal();
+gameOver.addEventListener('click', () => {
+  newGame();
+});
+
+newGame();
