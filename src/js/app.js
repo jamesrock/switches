e3781be3bae0958bd6f1ba681bd90467cd214b90
James Rock <hello@jamesrock.me>
2026-02-27 19:49:43 +0000

    updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,65 @@
 import '/css/app.css';
 import {
+  DisplayObject,
   setDocumentHeight,
-  formatTime, 
-  shuffle, 
-  formatNumber, 
-  pluckRandom, 
+  formatTime,
+  shuffle,
+  formatNumber,
+  pluckRandom,
   random,
-  createContainer,
+  makeContainer,
+  makeNode,
   makeArray
 } from '@jamesrock/rockjs';
+import interact from 'interactjs';
 
 setDocumentHeight();
 
 const app = document.querySelector('#app');
 const COUNT = 9;
 
-const createSwitch = () => {
-  const node = document.createElement('input');
-  node.type = 'checkbox';
-  return node;
+const makeIndex = (items, key = 'id') => {
+  const out = {};
+  items.forEach((item) => {
+    out[item[key]] = item;
+  });
+  return out;
+};
+
+class Item extends DisplayObject {
+  constructor(id, weight) {
+
+    super();
+
+    this.id = id;
+    this.weight = weight;
+    this.node = makeNode('div', 'item');
+
+    this.setProp('id', this.id);
+
+  };
+  setDropped(a) {
+
+    this.dropped = a;
+    this.setProp('dropped', this.dropped);
+    return this;
+
+  };
+  setChecked(a) {
+
+    this.checked = a;
+    return this;
+
+  };
+  setPosition(x, y) {
+
+    this.node.style.left = `${x}px`;
+    this.node.style.top = `${y}px`;
+    return this;
+
+  };
+  dropped = false;
+  checked = false;
 };
 
 const renderTotal = () => {
@@ -32,7 +73,7 @@ const renderTotal = () => {
 };
 
 const showGameOverScreen = () => {
-  switches.forEach((s) => {
+  items.forEach((s) => {
     s.disabled = true;
   });
   const duration = (Date.now() - time);
@@ -48,7 +89,7 @@ const showGameOverScreen = () => {
 
 const getMultipliers = () => {
   const multipliers = [];
-  switches.forEach((s, index) => {
+  makeArray(COUNT).forEach((s, index) => {
     multipliers.push(random((index)*10+1, ((index)*10)+10));
   });
   return shuffle(multipliers);
@@ -56,7 +97,7 @@ const getMultipliers = () => {
 
 const getValues = () => {
   const values = makeArray(COUNT, () => 0);
-  const bob = makeArray(COUNT, (a, index) => index);
+  const bob = makeArray(COUNT);
   makeArray(random(2, 8)).forEach(() => {
     values[pluckRandom(bob)] = 1;
   });
@@ -67,7 +108,7 @@ const generateSettings = () => {
   let value = 0;
   const values = getValues();
   const multipliers = getMultipliers();
-  switches.forEach((s, index) => {
+  makeArray(COUNT).forEach((s, index) => {
     value += (values[index] * multipliers[index]);
   });
   return {
@@ -79,64 +120,73 @@ const generateSettings = () => {
 
 const getTotal = () => {
   let bob = 0;
-  switches.forEach((slider, index) => {
-    bob += ((slider.checked ? 1 : 0) * settings.multipliers[index]);
+  items.forEach((item) => {
+    bob += ((item.checked ? 1 : 0) * item.weight);
   });
   return bob;
 };
 
-const switches = makeArray(COUNT, () => createSwitch());
+let items = null;
+let itemMap = null;
 let settings = null;
 let moves = 0;
 let time = null;
 
-const board = createContainer('board');
+const board = makeContainer('board');
 app.appendChild(board);
 
-const teller = createContainer('teller');
+const teller = makeContainer('teller');
 teller.dataset.active = false;
 // board.appendChild(teller);
 
-const display = createContainer('display');
+const display = makeContainer('display');
 display.innerText = 0;
 board.appendChild(display);
 
-const holder = createContainer('holder');
+const holder = makeContainer('holder');
 board.appendChild(holder);
 
-const gameOver = createContainer('game-over');
+const scales = makeContainer('scales');
+board.appendChild(scales);
+
+const gameOver = makeContainer('game-over');
 gameOver.dataset.active = false;
 board.appendChild(gameOver);
 
 const newGame = () => {
-  
+
+  if(false) {
+    items.forEach((item) => {
+      item.destroy();
+    });
+  };
+
   settings = generateSettings();
+
+  items = makeArray(COUNT, (a, id) => new Item(id, settings.multipliers[id]));
+  itemMap = makeIndex(items);
+
+  console.log(items);
 
   teller.innerHTML = `<div>${JSON.stringify(settings.values)}</div><div>${JSON.stringify(settings.multipliers)}</div>`;
 
   console.log(settings);
 
-  switches.forEach((s) => {
-    s.checked = false;
-    s.disabled = false;
+  items.forEach((item) => {
+    item.appendTo(holder);
   });
 
   gameOver.dataset.active = false;
 
   moves = 0;
   time = Date.now();
-  
+
   renderTotal();
 
 };
 
-switches.forEach((s) => {
-  holder.appendChild(s);
-});
-
 board.addEventListener('input', () => {
-  moves ++;
-  renderTotal();
+
 });
 
 gameOver.addEventListener('click', () => {
@@ -148,3 +198,62 @@ teller.addEventListener('click', () => {
 });
 
 newGame();
+
+let position = null;
+let group = null;
+
+interact('.item').draggable({
+	listeners: {
+		start(event) {
+
+      position = { x: event.target.offsetLeft, y: event.target.offsetTop };
+
+      console.log(event.target.dataset.id);
+
+      group = [itemMap[event.target.dataset.id]];
+      console.log(group);
+
+			group.forEach((item, index) => {
+        item.setDropped(false);
+			});
+
+		},
+    move(event) {
+
+			position.x += event.dx;
+			position.y += event.dy;
+			group.forEach((item, index) => {
+				item.setPosition((position.x), (position.y));
+      });
+
+		},
+	}
+});
+
+interact('.scales').dropzone({
+	accept: '.item',
+	ondrop: (event) => {
+
+    group.forEach((item) => {
+      item.setDropped(true).setChecked(true);
+    });
+
+    moves ++;
+    renderTotal();
+
+	}
+});
+
+interact('.items').dropzone({
+	accept: '.item',
+	ondrop: (event) => {
+
+    group.forEach((item) => {
+      item.setDropped(true).setChecked(false);
+    });
+
+    moves ++;
+    renderTotal();
+
+	}
+});
